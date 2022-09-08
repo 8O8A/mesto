749f3e6e5912283e7ea050d327d19fa1425328a2
Vladimir Levkin <vladimir231180@yandex.ru>
2022-09-08 20:28:30 +0300

    fix: исправить баги 1

--- scripts/test.js
+++ scripts/test.js
@@ -7,7 +7,7 @@ const profileAbout = document.querySelector('.profile__subtitle'); //род де
 
 const popupName = document.getElementById('form-name'); //имя пользователя в popup'e
 const popupAbout = document.getElementById('form-about'); //род деятельности пользователя в popup'e
-const popupSaveButton = overlayPopup.querySelector('.popup__save-button'); //кнопка "Сохранить" в popup'e
+// const popupSaveButton = overlayPopup.querySelector('.popup__save-button'); //кнопка "Сохранить" в popup'e
 
 
 // функция открытия и закрытия PopUp'a
@@ -15,39 +15,23 @@ function formToggle() {
     overlayPopup.classList.toggle('popup_opened');
 }
 
-
-
-// открываем PopUp'a со значениями "Имя" и "О себе" из профиля
+//открываем PopUp
 openEditButton.addEventListener('click', () => {
-    popupName.value = profileName.textContent;
-    popupAbout.value = profileAbout.textContent;
     formToggle();
-});
+})
 
-// закрываем PopUp'a
+// закрываем PopUp
 closePopupButton.addEventListener('click', () => {
     formToggle();
-});
+})
+
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
     profileName.textContent = popupName.value;
     profileAbout.textContent = popupAbout.value;
+    formToggle();
 }
 
 overlayPopup.addEventListener('submit', formSubmitHandler);
 
-
-
-// сохраняем изменения в popup и закрываем
-popupSaveButton.addEventListener('click', () => {
-    formToggle();
-})
-
-// Ставим лайки фотографиям
-// const photoLike = document.querySelectorAll('.gallery__like-button');
-//     for (let i=0; i < photoLike.length; i = i + 1) {
-//         photoLike[i].addEventListener('click', () => {
-//             photoLike[i].classList.toggle('gallery__like-button_active');
-//         });
-//     }
